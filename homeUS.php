27dc62e25f502496de0e9f467735062e47535006
siferis <karimraam1x@gmail.com>
2018-09-27 13:00:16 -0500

    navbar
    carpeta menu se creo el menu y  se carga en las de "home"

---
+++ homeUS.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Comercializadora</title>
+    <!--Librerias-->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/stilos.css">
+    <link href="webcodecamjs-master/css/style.css" rel="stylesheet">
+  </head>
+  <style media="screen">
+  nav.navbar {
+      background: rgba(0, 0, 0, 0.7);
+  }
+  .navbar-default .navbar-nav .open .dropdown-menu > .active > a,
+.navbar-default .navbar-nav .open .dropdown-menu > .active > a:hover,
+.navbar-default .navbar-nav .open .dropdown-menu > .active > a:focus {
+    color: rgba(0, 0, 0, 0.7);
+    background-color: rgba(0, 0, 0, 0.7);
+}
+
+
+  </style>
+  <body background="img/1.jpg" background-repeat: no-repeat>
+    <?php
+    include("menu/menuUsua.php");
+     ?>
+
+
+    <!-- librerias-->
+    <script src="js/jquery-1.12.4.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ menu/menuAdmin.php
@@ -0,0 +1,43 @@
+
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#" style="color:#FFFFFF";>Inicio</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="dropdown barra">
+              <a  style="color:#FFFFFF"; href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vistas <span class="caret"></span></a>
+              <ul class="dropdown-menu hola">
+                <li><a href="#">Solicitud de Servicios</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Comentarios</a></li>
+              </ul>
+            </li>
+            <li class="dropdown barra">
+              <a  style="color:#FFFFFF"; href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Encuestas <span class="caret"></span></a>
+              <ul class="dropdown-menu hola">
+                <li><a href="#">Ver Encuestas</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Modificar Encuestas</a></li>
+
+              </ul>
+            </li>
+
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li ><a style="color:#FFFFFF"; href="#">Salir</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
